4ee4765837df3915afdc90395683ee97d18d8e6b
Junjie He <j247he@uwaterloo.ca>
2020-06-07 22:41:58 -0400

    A1.1 Implement Lock

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -76,6 +76,10 @@ struct lock {
         char *lk_name;
         // add what you need here
         // (don't forget to mark things volatile as needed)
+        bool volatile held;
+        struct thread * owner;
+        struct wchan * wchan;
+        struct spinlock spinlock;
 };
 
 struct lock *lock_create(const char *name);
